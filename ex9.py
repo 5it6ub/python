81283c9010bc4cdae43a1c0b69c16172afed3b87
Shungoh Kaetsu <boss.gori@gmail.com>
2013-07-07 09:58:03 +0800

    Adds ex7.py ex8.py ex9.py

---
+++ ex9.py
@@ -0,0 +1,17 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+months_rest = "Sep\nOct\nNov\nDec"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+print "Here are the rest of months: %r" % months_rest
+
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+
