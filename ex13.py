669197435824037b857c6347e9790a5774454c76
Shungoh Kaetsu <boss.gori@gmail.com>
2013-07-23 16:10:53 +0800

    Adds ex13 & 14.py

---
+++ ex13.py
@@ -0,0 +1,20 @@
+from sys import argv
+
+script, first, second, third = argv
+
+#print "The script is called:", script
+#print "Your first variable is:", first
+#print "Your second variable is:", second
+#print "Your thrid variable is:", third
+
+print "Your first variable is:", first
+a_var = raw_input('Enter a value:\t')
+print 'a_var is', a_var
+
+print "Your first variable is:", second
+b_var = raw_input('Enter a value:\t')
+print 'b_var is', b_var
+
+print "Your first variable is:", third
+c_var = raw_input('Enter a value:\t')
+print 'c_var is', c_var
